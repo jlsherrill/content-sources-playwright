4ac9abf170395fee6d7790bb17f0972b5108830b
adewar <andrewgdewar@gmail.com>
2025-02-05 13:09:29 -0700

    Update scripts to pull from main for both backend and front-end

--- .github/workflowScripts/getBackendTests.sh
+++ .github/workflowScripts/getBackendTests.sh
@@ -2,9 +2,9 @@
 
 # Variables
 REPO_URL="https://github.com/content-services/content-sources-backend.git"
-BRANCH="playwright-POC"
-FOLDER_PATH="_playwright-tests/tests/API"
-LOCAL_DIR="tests/"
+BRANCH="main"
+FOLDER_PATH="API"
+LOCAL_DIR="tests"
 
 # Create a temporary directory
 TEMP_DIR=$(mktemp -d)
@@ -16,8 +16,7 @@ rm -rf $LOCAL_DIR/$FOLDER_PATH
 git clone --branch $BRANCH $REPO_URL $TEMP_DIR
 
 # Copy the specific folder to the local directory
-mkdir -p $LOCAL_DIR
-cp -r $TEMP_DIR/$FOLDER_PATH $LOCAL_DIR
+cp -r $TEMP_DIR/_playwright-tests/tests/$FOLDER_PATH $LOCAL_DIR
 
 # Clean up the temporary directory
 rm -rf $TEMP_DIR
--- .github/workflowScripts/getFrontendTests.sh
+++ .github/workflowScripts/getFrontendTests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Variables
-REPO_URL="https://github.com/Andrewgdewar/content-sources-frontend.git"
-BRANCH="HMS-5287"
-FOLDER_PATH="_playwright-tests/UI"
-LOCAL_DIR="tests/"
+REPO_URL="https://github.com/content-services/content-sources-frontend.git"
+BRANCH="main"
+FOLDER_PATH="UI"
+LOCAL_DIR="tests"
 
 # Create a temporary directory
 TEMP_DIR=$(mktemp -d)
@@ -16,8 +16,7 @@ rm -rf $LOCAL_DIR/$FOLDER_PATH
 git clone --branch $BRANCH $REPO_URL $TEMP_DIR
 
 # Copy the specific folder to the local directory
-mkdir -p $LOCAL_DIR
-cp -r $TEMP_DIR/$FOLDER_PATH $LOCAL_DIR
+cp -r $TEMP_DIR/_playwright-tests/$FOLDER_PATH $LOCAL_DIR
 
 # Clean up the temporary directory
 rm -rf $TEMP_DIR
